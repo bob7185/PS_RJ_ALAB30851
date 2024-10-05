f59007f0809bd0164083cccfc65568fd84d03a11
Robert_Jean <stevejean285@gmail.com>
2024-10-04 21:30:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+https://github.com/bob7185/PS_RJ_ALAB30851
+# per scholas JS Alab 308.5.1
+
+ Creating reusable functions
+
---
+++ index.js
@@ -0,0 +1,33 @@
+// function that takes an array of numbers  and returns the sum
+function arraySum(arr)
+{ 
+    let sum = 0;
+    for (let i =0; i < arr.length; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+// Function that returne the average of an array of numbers
+function  arrayAverage(arr)
+{
+    let sum = 0;
+    for (let i =0; i < arr.length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum/arr.length;
+}
+
+//Function that takes an array of strings and returns the longest string 
+function longestString(arr){
+let index = 0;
+
+
+}
+
+
+
+const array = [1,2,3,4,5,6,7,8,9]
+console.log(arraySum(array))
